fd7dddb3326b2b0e295cf7394489585c91fac7cd
JPZU <juanpablozp@icloud.com>
2024-12-03 14:36:26 -0500

    Completed ClientService, method's are OK

--- src/main/java/com/macalz/macalz_backend/web/controller/ClientController.java
+++ src/main/java/com/macalz/macalz_backend/web/controller/ClientController.java
@@ -5,9 +5,7 @@ import com.macalz.macalz_backend.domain.service.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,5 +18,33 @@ public class ClientController {
     @GetMapping("/all")
     public ResponseEntity<List<ClientDTO>> getAll() {
         return new ResponseEntity<>(clientService.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/name/{name}")
+    public ResponseEntity<ClientDTO> getByName(@PathVariable("name") String name) {
+        return clientService.findByName(name)
+                .map(client -> new ResponseEntity<>(client, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @GetMapping("/id/{id}")
+    public ResponseEntity<ClientDTO> getById(@PathVariable("id") String clientId) {
+        return clientService.findById(clientId)
+                .map(client -> new ResponseEntity<>(client, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<ClientDTO> save(@RequestBody ClientDTO client) {
+        return new ResponseEntity<>(clientService.save(client), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Void> deleteClientById(@PathVariable("id") String clientId) {
+        if (clientService.delete(clientId)) {
+            return new ResponseEntity<>(HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 }
